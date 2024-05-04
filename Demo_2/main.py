cfc604f801ecbb2d794a893337b20de319e2cc83
sbendavid <samuelbendavid01@gmail.com>
2024-05-04 21:39:53 +0100

    Created the second demo

--- Demo_2/main.py
+++ Demo_2/main.py
@@ -1,42 +1,44 @@
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
-# model.add(tf.keras.layers.Flatten())
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 # model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 # model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-# model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+# model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 # model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# model.fit(x_train, y_train, epochs=3)
 
-# val_loss, val_acc = model.evaluate(x_test, y_test)
-# print(val_loss, val_acc)
+# model.fit(x_train, y_train, epochs=20)
 
-# model.save('handwritten.keras')
+# model.save('handwritten.model.keras')
 
-model = tf.keras.models.load_model('handwritten.keras')
+# loss, accuracy = model.evaluate(x_test, y_test)
+# print(loss)
+# print(accuracy)
 
-# new_model = tf.keras.models.load_model('handwritten.model')
+model = tf.keras.models.load_model('handwritten.model.keras')
 
-# predictions = new_model.predict([x_test])
-# print(predictions)
-
-# print(np.argmax(predictions[0]))
-
-# plt.imshow(x_test[0])
-# plt.show()
-
-loss, accuracy = model.evaluate(x_test, y_test)
-print(loss)
-print(accuracy)
+image_number = 1
+while os.path.isfile(f'digits/digit_{image_number}.png'):
+    try:
+        img = cv2.imread(f'digits/digit_{image_number}.png')[:, :, 0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f'This digit is probably a {np.argmax(prediction)}')
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print('Error')
+    finally:
+        image_number += 1
